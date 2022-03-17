35dfd90e31d22786dd05fb11768cbc8e34f6d0fa
remayuen <susanamer54@gmail.com>
2022-03-17 20:10:04 +0300

    added js folder

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// DESIGN TOGGLE EFFECT
+
+$('.target-design').click(function() {
+  $(".design-showing").toggle();
+  $(".design-hidden").toggle();
+});
+
+
+// DEVELOPMENT TOGGLE EFFECT
+
+$('.target-dev').click(function() {
+  $(".dev-showing").toggle();
+  $(".dev-hidden").toggle();
+});
+
+
+// PRODUCT TOGGLE EFFECT
+
+$('.target-product').click(function() {
+  $(".product-showing").toggle();
+  $(".product-hidden").toggle();
+});
